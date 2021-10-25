3bb78e702efc9f6cd8c9049422665d4049710594
Ibrahim Hossain <outsideworkibrahim@gmail.com>
2021-10-25 22:22:54 +0600

    working on checkbox

--- core/static/app/js/paymentPage.js
+++ core/static/app/js/paymentPage.js
@@ -11,13 +11,13 @@ function payment(evt, paymentName) {
     document.getElementById(paymentName).style.display = "block";
     evt.currentTarget.className += " active";
   }
-
 document.getElementById("defaultOpen").click();
 
+
+
+//function for Making Procced To Pay button disabled if not select any option ....
 var PTPayButton = document.getElementById('PTPayBtn')
-
 var cODElement = document.getElementById('cODCheckbox')
-
 PTPayButton.addEventListener('mouseover', function(){
   if (cODElement.checked)
   {console.log("now you will be redirected to the next page")}
@@ -25,10 +25,23 @@ PTPayButton.addEventListener('mouseover', function(){
     console.log("you must select one option above")
     let selectionError = document.getElementById('selectionError')
     selectionError.innerHTML = "You must select an option"
-
+    PTPayButton.setAttribute('disabled', "disabled")
+  }
+})
+cODElement.addEventListener('change', function(){
+  if (cODElement.checked)
+  { PTPayButton.removeAttribute('disabled')
+    document.getElementById('selectionError').innerHTML = ""
   }
 })
 
-PTPayButton.addEventListener('mouseout', function(){
-document.getElementById('selectionError').innerHTML = ""
-})
+
+// making the choice function to select only one choice......
+var curierSelector = document.getElementsByClassName("curier")
+let len = curierSelector.length
+for (i = 0; i < len; i++){
+  if(curierSelector[i].checked == true){
+    console.log("at least one has been selected")
+  }
+  
+}
