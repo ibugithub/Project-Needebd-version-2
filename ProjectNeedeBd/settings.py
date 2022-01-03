0fec33f95a67d2e4ba35faf89f40af01ef91b561
Ibrahim Hossain <outsideworkibrahim@gmail.com>
2022-01-03 08:39:51 +0600

    nothing

--- ProjectNeedeBd/settings.py
+++ ProjectNeedeBd/settings.py
@@ -79,23 +79,23 @@ WSGI_APPLICATION = 'ProjectNeedeBd.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'data.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'needebd database',
-        'USER': 'postgres',
-        'PASSWORD': 'needebd71%',
-        'HOST':'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'needebd database',
+#         'USER': 'postgres',
+#         'PASSWORD': 'needebd71%',
+#         'HOST':'localhost',
+#         'PORT': '5432'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
