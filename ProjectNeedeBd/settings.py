9432ebb73512291ca8e4d9b3906f2d6ceb84c7bf
Ibrahim Hossain <outsideworkibrahim@gmail.com>
2022-01-03 08:15:12 +0600

    nothing

--- ProjectNeedeBd/settings.py
+++ ProjectNeedeBd/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-gf9sv19irw^96_9*qxr4&zybrx&=6%y!vvx&l1m2f@eyn7oc!j
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','needebd.herokuapp.com']
 
 # Application definition
 INSTALLED_APPS = [
