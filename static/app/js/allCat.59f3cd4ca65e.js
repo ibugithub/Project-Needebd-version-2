b0302af7daba26a4527dd383b4a6736d7d63b9c0
Ibrahim <outsideworkibrahim@gmail.com>
2022-01-05 20:04:57 +0600

    forgot to add configvar

--- static/app/js/allCat.59f3cd4ca65e.js
+++
@@ -1,8 +0,0 @@
-
-function scrollFunc(elm){
-  catName = elm.getAttribute('targetElm')
-  const targetElm = document.getElementById(catName)
-
-  console.log(targetElm)
-  $('html').animate({scrollTop: targetElm.offsetTop-90});
-}
