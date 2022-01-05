d65cca6a081afccc92fe39b0fec9288c68bd1fd3
Ibrahim <outsideworkibrahim@gmail.com>
2022-01-05 18:34:07 +0600

    divide the virtual env

--- ProjectNeedeBd/settings.py
+++ ProjectNeedeBd/settings.py
@@ -22,11 +22,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gf9sv19irw^96_9*qxr4&zybrx&=6%y!vvx&l1m2f@eyn7oc!j'
+# # SECURITY WARNING: don't run with debug turned on in production!
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# This is for Local Development
+SECRET_KEY = 'django-insecure-gf9sv19irw^96_9*qxr4&zybrx&=6%y!vvx&l1m2f@eyn7oc!j'
 DEBUG = True
 ALLOWED_HOSTS = []
+
+
+# This is for Production Development
+# SECRET_KEY = os.environ["SECRET_KEY"]
+# DEBUG = False
+# ALLOWED_HOSTS = ['localhost','needebd.herokuapp.com']
 
 # Application definition
 INSTALLED_APPS = [
@@ -79,23 +86,23 @@ WSGI_APPLICATION = 'ProjectNeedeBd.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'data.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'needebd database',
-        'USER': 'postgres',
-        'PASSWORD': 'needebd71%',
-        'HOST':'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'needebd database',
+#         'USER': 'postgres',
+#         'PASSWORD': 'needebd71%',
+#         'HOST':'localhost',
+#         'PORT': '5432'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
