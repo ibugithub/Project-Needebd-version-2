26e5a5efc0b43a6f5af1dc591ec8cae92a800ae8
Ibrahim <outsideworkibrahim@gmail.com>
2022-01-05 13:30:44 +0600

    heroku deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT ProjectNeedeBd.wsgi:application
--- ProjectNeedeBd/settings.py
+++ ProjectNeedeBd/settings.py
@@ -22,11 +22,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gf9sv19irw^96_9*qxr4&zybrx&=6%y!vvx&l1m2f@eyn7oc!j'
+# # SECURITY WARNING: don't run with debug turned on in production!
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1','needebd.herokuapp.com']
+# This is for Local Development
+# SECRET_KEY = 'django-insecure-gf9sv19irw^96_9*qxr4&zybrx&=6%y!vvx&l1m2f@eyn7oc!j'
+# DEBUG = True
+# ALLOWED_HOSTS = []
+
+
+# This is for Production Development
+SECRET_KEY = os.environ["SECRET_KEY"]
+DEBUG = False
+ALLOWED_HOSTS = ['localhost','needebd.herokuapp.com']
 
 # Application definition
 INSTALLED_APPS = [
