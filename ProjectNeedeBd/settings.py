a9cdf49d665a2a9502db762b55e092d2f5e1f3f9
Ibrahim <outsideworkibrahim@gmail.com>
2022-03-05 07:37:18 +0600

    added postgres info to the digital ocean server

--- ProjectNeedeBd/settings.py
+++ ProjectNeedeBd/settings.py
@@ -90,24 +90,24 @@ WSGI_APPLICATION = 'ProjectNeedeBd.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'needebd_database',
-#         'USER': 'needebd',
-#         'PASSWORD': 'needebd71%database',
-#         'HOST':'localhost',
-#         'PORT': ''
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'needebd_database',
+        'USER': 'needebd',
+        'PASSWORD': 'needebd71%database',
+        'HOST':'localhost',
+        'PORT': ''
+    }
+}
 
 
 # DATABASES = {
