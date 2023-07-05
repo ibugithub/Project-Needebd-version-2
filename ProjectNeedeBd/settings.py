579633d9329dc61de3e4848bc07b6f08e5a6bfff
ibugithub <outsideworkibrahim@gmail.com>
2023-07-05 13:28:35 +0600

    update pillow and psyg2

--- ProjectNeedeBd/settings.py
+++ ProjectNeedeBd/settings.py
@@ -90,27 +90,27 @@ WSGI_APPLICATION = 'ProjectNeedeBd.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
-
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'needebddatabase',
-        'USER': 'needebddb',
-        'PASSWORD': 'needebd71dbuser',
-        'HOST':'localhost',
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'needebddatabase',
+#         'USER': 'needebddb',
+#         'PASSWORD': 'needebd71dbuser',
+#         'HOST':'localhost',
+#         'PORT': ''
+#     }
+# }
 
 
 # DATABASES = {
